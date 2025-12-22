b56ee930573ab1923664ad82fe265de63409fb2f
starswaterbrook <mwkm00@gmail.com>
2025-12-22 12:30:47 +0100

    feat: include all standard modules to the root init for imports

--- src/find_anything/__init__.py
+++ src/find_anything/__init__.py
@@ -1,3 +1,18 @@
+from find_anything.embedding_repository import BaseEmbeddingRepository
+from find_anything.feature_encoder import DinoV2FeatureEncoder
+from find_anything.mask import DenseFeatureMaskPooler, FastSAMMaskGenerator, TopKMaskSelector
+from find_anything.models import MaskEmbedding, MatcherResult
 from find_anything.object_matcher import ZeroShotObjectMatcher
+from find_anything.utils import get_object_image_paths
 
-__all__ = ["ZeroShotObjectMatcher"]
+__all__ = [
+    "BaseEmbeddingRepository",
+    "DenseFeatureMaskPooler",
+    "DinoV2FeatureEncoder",
+    "FastSAMMaskGenerator",
+    "MaskEmbedding",
+    "MatcherResult",
+    "TopKMaskSelector",
+    "ZeroShotObjectMatcher",
+    "get_object_image_paths",
+]
